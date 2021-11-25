a28a78cfa58d059693d3aa9343f7118a55aecccc
Aaron Mineen <amineen64@hotmail.com>
2021-11-25 21:28:44 +0000

    Added table to display kpi data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React from 'react';
+import Box  from "@mui/material/Box"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { substationKpis } from './data';
 
 const App = () => {
+
   return (
-    <div>
-      Hello There
-    </div>
+    <Box component="h2">
+     <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Quarter</TableCell>
+            <TableCell align="right">Installed Capacity(MW)</TableCell>
+            <TableCell align="right">Peak Load (MW)</TableCell>
+            <TableCell align="right">Customers Served</TableCell>
+            <TableCell align="right">Total Generation (MWH)</TableCell>
+            <TableCell align="right">Total Operation Time (hrs)</TableCell>
+            <TableCell align="right">Customers Interrupted </TableCell>
+            <TableCell align="right">Duration of Interruption (hrs) </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {substationKpis.map((data) => (
+            <TableRow
+              key={data.quarter}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {data.name}
+              </TableCell>
+              <TableCell align="right">{data.quarter}</TableCell>
+              <TableCell align="right">{data.installedCapacity}</TableCell>
+              <TableCell align="right">{data.peakLoad}</TableCell>
+              <TableCell align="right">{data.totalCustomersServed}</TableCell>
+              <TableCell align="right">{data.energyDelivered}</TableCell>
+              <TableCell align="right">{data.uptime}</TableCell>
+              <TableCell align="right">{data.totalCustomersInterrupted}</TableCell>
+              <TableCell align="right">{data.durationOfInterruption}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Box>
   )
 }
 
